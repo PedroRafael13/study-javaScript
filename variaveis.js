fdb62de0d867f2cd1176de93bd7ea4fde04b1609
PedroRafael13 <pedrorafaeldsn12@gmail.com>
2024-07-09 11:16:08 -0400

    revisando o conteudo de js

---
+++ variaveis.js
@@ -0,0 +1,22 @@
+/**  
+
+* Todo: Tipo de variaveis e seus significados
+** Existe 3 tipos de variaveis, são elas => {
+  *! LET, const e var 
+}
+
+* Todo : Significado de cada um 
+*! LET => Pode ter o valor alterado, quando quiser
+*! const => O valor NÃO pode ser declarado novamnete 
+*! Var => Não é mais utilizado 
+
+*/
+
+
+let testeOne = 30
+
+testeOne = 50
+
+const testSecond = 100
+
+console.log(testSecond)

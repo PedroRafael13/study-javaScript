057bccfd103b17c72e86fd3bd7cc7fc8e71bdd85
PedroRafael13 <pedrorafaeldsn12@gmail.com>
2025-02-17 18:04:31 -0400

    treinando

---
+++ exercise/moduleOne/exercise002.js
@@ -0,0 +1,10 @@
+/*: 2. Escreva um programa que calcule a média de três números e exiba o resultado no console. 
+Para calcular a média você só precisa somar os 3 números e dividir por 3. */
+
+const numberOne = 1
+const numberTwo = 9
+const numberThree = 4
+
+const result = numberOne + numberTwo + numberThree / 2
+
+console.log(result)
